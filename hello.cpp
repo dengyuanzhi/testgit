9c8004219a24df41bc27cfc7c2fea279948b2181
jankin <archkernel@outlook.com>
2019-10-22 20:08:29 +0800

    新建了一个hello类，没有测试

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <string>
 
+class Hello
+{
+	
+private:
+	std::String name;
+	std::String age;
+	std::String sex;
+public:
+	void sayHello(){
+		std::cout <<"name:"
+			<< this.name <<"\t"
+			<< "Age:"
+			<< this.age << "\t"
+			<< "Sex:"
+			<< this.sex<<std::endl;
+	}
+	Hello(std::String name,std::String age,sdt::String sex){
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+	}
+}
 
 int main()
 {
